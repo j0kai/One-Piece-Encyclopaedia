3dd5bee7f64dccd769d2cb6ffd6e601ea84bd1d8
Kyle Jones <kyjo1308@gmail.com>
2022-07-31 18:52:49 +0100

    First commit - committing all files to the repository.

---
+++ Pages/Characters.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace One_Piece_Encyclopedia.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
